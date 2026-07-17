b81ec2e681e7d2df21d4cc8eca8f132708dfc938
AJMacadoodle <whowhatwhere2020@yahoo.com>
2026-07-17 18:24:18 +0000

    Add dependency configuration files for Streamlit Cloud

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="traffic-advisor",
+    version="0.1.0",
+    install_requires=[
+        "streamlit>=1.59.2",
+        "transformers>=4.30.0",
+        "torch>=2.0.0",
+    ],
+)
